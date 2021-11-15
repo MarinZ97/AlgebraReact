4aaa6adce340f9f623d4644fc0f2847dff06cafb
programer10 <programer10@ucione.local>
2021-11-15 21:13:43 +0100

    Dodaj prepravku

--- src/ChildVjezba.js
+++ src/ChildVjezba.js
@@ -4,8 +4,8 @@ import React from 'react'
 export default function Child({children, name, age}) {
     return <div>
     <h2>Child</h2>
-    <p>{name}</p>
-    <p>{age}</p>
+    <p>Ime: {name}</p>
+    <p>Godine: {age}</p>
     <p>{children}</p>
     </div>
 }
