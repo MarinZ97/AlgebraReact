f6789c1d7da6e56e47b1dd9bd2de7e4076a47353
programer10 <programer10@ucione.local>
2021-12-01 21:15:06 +0100

    dodaj

--- src/User.js
+++ src/User.js
@@ -1,8 +1,9 @@
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 
-export default function({name, age}) {
+export default function({name, onRename}) { 
     return(<div>
         <p>Pozdrav, moje ime je {name} i imam {age} godina.</p>
-        <input type="text" />
+        <input type="text" value={user.name} onChange={(e) => onRename(e.target.value)} />
     </div>)
 }
