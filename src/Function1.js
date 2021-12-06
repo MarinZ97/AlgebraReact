f1b979a67a9b2350c8854c65846879b195534151
marin <zuna20@gmail.com>
2021-12-06 22:56:41 +0100

    Vjezbanje dodaj godine

--- src/Function1.js
+++ src/Function1.js
@@ -3,15 +3,25 @@ import User1 from './User1';
 
 export default function Function1() {
     const [data, setData] = useState([
-                {ime:'Janko', godine:'22'},
-                {ime:'Tina', godine:'37'}]);
+                {ime:'Janko', godine: 22},
+                {ime:'Tina', godine: 37}]);
 
-    
-
+   
+     const changeName = () => {
+        const newName = [];
+        for(let i = 0; i < data.length; i++) {
+        const listName = {ime: data[i].ime, godine: data[i].godine + 1};
+        newName.push(listName);
+        }
+        setData(newName);
+    }
 
     return (<div>
         <h1>Vjezbanje</h1>
-        <User1 pod={data[0]}/>
-        <button>Click me!</button>
-    </div>)
+
+        <User1 pod={data[0]} />
+        <User1 pod={data[1]} />
+
+        <button onChange={changeName}>Click me!</button>
+    </div>);
 }
--- src/User1.js
+++ src/User1.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-export default function User1({pod}) {
+export default function User1({pod, onRename}) {
+
+    const handleChange = (event) => {
+        const novoIme = event.target.value;
+        onRename(novoIme);
+    }
 
     return (<div>
         <div>Ja sam {pod.ime} i imam {pod.godine} godine</div>
-        <input></input>
+        <input value={pod.name} onChange={handleChange} disabled={!onRename} />
+        
     </div>)
 }
