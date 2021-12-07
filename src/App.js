5ef1df88ffb59f98a4baa7425737fe2640f7a438
programer10 <programer10@ucione.local>
2021-12-07 19:38:24 +0100

    boostrap, map

--- src/App.js
+++ src/App.js
@@ -18,3 +18,20 @@ export default App;
 componentWillMount se više ne koristi!
 react lifecycle diagram -> pogledat i zapamtit dijagram
 */
+
+/*
+const varijabla = kopiaUser.map(x=>{  kao "i" kod for petlje x predstavlja value koji se dodaje
+{...x, godine:x.godine + 1}})
+
+const korisnik = {ime: users.ime, godine.users.godine}
+const noviKorisnici = {...users, korisnik} 
+setUsers(noviKorisnici)
+
+je isto kao i 
+const noviKorisnici = {...users}
+noviKorisnici.push(korisnik)
+
+!!!!!!!!!!
+dodavanje boostrapa u react
+boostrop-react pa vidjeti!!
+*/
