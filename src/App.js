36fc6eb7b17927d614d40df154c0e8849c4605c8
programer10 <programer10@ucione.local>
2021-12-13 20:48:09 +0100

    dodaj tekst

--- src/App.js
+++ src/App.js
@@ -19,11 +19,8 @@ class App extends React.Component {
       return <div>Loading NASA data</div>
     }
 
-    // Ovaj if može biti u render u ovom slučaju jer ili će biti samo ovo 
-    // gore ili donji return, ali u našem slučaju if samo stoji dok se ne 
-    // učita drugio dio
    
-    const { title, url, explanation } = this.state.data; // izvuci tittle, url.. iz data
+    const { title, url, explanation } = this.state.data; 
     return (
       <div>
         <h1>NASA picture of the day</h1>
@@ -38,37 +35,13 @@ class App extends React.Component {
 export default App;
 
 /*
-Fetch API
-Imamo samo jedan url, stavimo Json koji radi response na body di 
-upišemo putanju kaj želimo izvući iz objekata sta trazimo
 
-// Ovo je za GET method
-fetch(url) 
-.then(response => response.json()) paket od response parsiramo u json i zato ide opet .then jer postaje promise 
-.then(data => console.log(data)) i u ovoj zagradi onda radimo funkciju da odraduje sta trebamo
-
-Ako želimo useEffect koristit samo jednom stavimo na kraju [], da će se izvršiti samo jednom i tjt kao DidIMount 
-
-Imenik* ZADAĆA
-Zapisat podatke u stateu i iz njega izvući podatke
-prezime,ime, broj mobitela/telefona
-
-Da prikaže abecedu gore i da kad se stisne slovo abecede da ispiše ta prezimena koja počinju na to slovo
-
-da ispiše 3 inputa 
-ime / prezime  / tel  i da sastrane bude X da može obrisati i olokvka da može edit
-(edit) -> da se može promjeniti ime na postojećem inputu i druge stavke pa ažurirati
-
------------------------
-
-REDUX
-- Mora se instalirati redux paket i react-redux
-- Uvijek se pise type: / i action.type
-- Nesmije se izlazit iz "reducer" funckije, ona radi samo ono sta je dobila i vrati rezultat
-- Importa se { connect } da se poveze sa redux file da se prebace podaci
-- Connect uzima app i napravi novu komponentu oko tog app i pravimo se da je to app i connect exportamo
-
-UseEffect se stavi nešto iz state-a inače
+- UseEffect se stavi nešto iz state-a inače, ona se zove kada se učitava komponenta i kada
+ se pozove kada se odradi nešto, uglate zagrade označavaju kada će se drugi put zvat, ako 
+ su prazne onda uvijek na promjenu, ako je nešto iz statea onda kada se to promjeni
+- keyCode - 13 je enter pa da ne pišemo string napišemo broj
+- ? predstavlja da provjerava ako nešto nije null onda radi poslije uputnika, a koje null onda ništa ne radi
+npr. -> user?.map() 
 
 
 */
